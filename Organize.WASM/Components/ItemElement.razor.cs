33850ae1ed358ac48feb29b2df362130a3e164cd
rogerccopa <rogerccopa@gmail.com>
2021-09-02 16:04:42 -0500

    Initial Item Elements layout

---
+++ Organize.WASM/Components/ItemElement.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Organize.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Components
+{
+    public partial class ItemElement<TItem> : ComponentBase where TItem: BaseItem
+    {
+        [Parameter]
+        public TItem Item { get; set; }
+    }
+}
