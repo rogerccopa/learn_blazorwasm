6c7653bbf232e32b904d023d2111add0eaa9c5e1
rogerccopa <rogerccopa@gmail.com>
2021-09-02 17:14:15 -0500

    ItemCheckBoxes

---
+++ Organize.WASM/Components/ItemCheckBoxBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Organize.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organize.WASM.Components
+{
+    public class ItemCheckBoxBase : ComponentBase
+    {
+        [Parameter]
+        public BaseItem Item { get; set; }
+    }
+}
