920aa75bedb0ecbf4b10ee7d1d21095029972093
rogerccopa <rogerccopa@gmail.com>
2021-09-27 08:45:52 -0500

    Retrieve items from Service

--- Organize.WASM/Components/ItemEdit.razor.cs
+++ Organize.WASM/Components/ItemEdit.razor.cs
@@ -1,4 +1,6 @@
-﻿using Organize.Shared.Entities;
+﻿using Microsoft.AspNetCore.Components;
+using Organize.Shared.Entities;
+using Organize.WASM.ItemEdit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,23 @@ namespace Organize.WASM.Components
 {
     public partial class ItemEdit
     {
+        [Inject]
+        private ItemEditService ItemEditService { get; set; }
+
         private BaseItem Item { get; set; } = new BaseItem();
         private int TotalNumber { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            ItemEditService.EditItemChanged += HandleEditItem;
+            Item = ItemEditService.EditItem;
+        }
+
+        private void HandleEditItem(object sender, ItemEditEventArgs e)
+        {
+            Item = e.Item;
+            StateHasChanged();
+        }
     }
 }
